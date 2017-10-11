f093397ee65806c68103957688bd2a28c3ee7edb
wujiaming <1114802868@qq.com>
2017-10-11 11:06:08 +0800

    ""

--- auto.sh
+++ auto.sh
@@ -1,15 +1,19 @@
-#!/usr/bin/expect
+#!/bin/bash
+
+dir_full=`pwd`
+dir=`basename $dir_full`
+
+/usr/bin/expect << EOF
 
 set timeout 20
-set name [lindex $argv 0]
-set commit [lindex $argv 1]
-set passwd [lindex $argv 2]
+set commit [lindex $argv 0]
+set passwd [lindex $argv 1]
 spawn git add .
 spawn git commit -m"$commit"
-spawn git push https://github.com/wujiaming123/${name}.git master
+spawn git push https://github.com/wujiaming123/${dir}.git master
 expect "'https://github.com':"
 send "wujiaming123\n"
 expect "'https://wujiaming123@github.com':"
 send "${passwd}\n"
 expect eof
-exit
+EOF
