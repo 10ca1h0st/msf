55e5f2067549976c2e29d5f155b6a21efe1831a1
wujiaming <1114802868@qq.com>
2017-10-11 09:48:59 +0800

    "it is a auto test"

--- auto.sh
+++ auto.sh
@@ -3,12 +3,13 @@
 set timeout 20
 set name [lindex $argv 0]
 set commit [lindex $argv 1]
+set passwd [lindex $argv 2]
 spawn git add .
 spawn git commit -m"$commit"
 spawn git push https://github.com/wujiaming123/${name}.git master
 expect "'https://github.com':"
 send "wujiaming123\n"
 expect "'https://wujiaming123@github.com':"
-send "wujiaming123456\n"
+send "${passwd}\n"
 expect eof
 exit
